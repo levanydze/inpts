d1ad68ff89e9a5c081c88ceeb4c9d75eadacf8fa
levanydze <66974840+levanydze@users.noreply.github.com>
2024-03-22 19:15:15 +0100

    1

--- src/app/firebase.js
+++ src/app/firebase.js
@@ -7,15 +7,15 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB1kNiFHFsBgwyoy8dKQzv15lFVnDGFbUk",
-  authDomain: "chacha-9137b.firebaseapp.com",
-  databaseURL:
-    "https://chacha-9137b-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "chacha-9137b",
-  storageBucket: "chacha-9137b.appspot.com",
-  messagingSenderId: "709224516273",
-  appId: "1:709224516273:web:4db08b25f54b09a201b090",
-  measurementId: "G-49FPQ0Z1NX",
+  apiKey: process.env.NEXT_PUBLIC_REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId:
+    process.env.NEXT_PUBLIC_REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
--- src/app/post/CreateNewItem.tsx
+++ src/app/post/CreateNewItem.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import styles from "./CreateNewItem.module.css";
 import Image from "next/image";
@@ -70,148 +70,150 @@ export default function CreateNewItem({
   setNewItemValue,
   setDisableValue,
 }: CreateNewItemProps) {
+  const [isCreateModalOpen, setIsCreateModalOpen] = useState<boolean>(false);
   return (
     <div>
-      <form
-        onSubmit={saveData}
-        className="p-4 items-center flex flex-col border-4 border-teal-800 m-4 "
-      >
-        <div className="m-2">
-          <h2 className="text-white text-2xl mb-4 text-center">
-            Create New List For Your Menu
-          </h2>
-        </div>
-        <div className={styles.allInputWrapper}>
-          <div className={styles.selectCategoryInput}>
-            <h3>Category</h3>
-            <div className="selectWrapper">
-              <select
-                value={menuCategoryValue}
-                onChange={(e) => handleMenuCategoryChange(e.target.value)}
-              >
-                <option className="flex justify-center" value="">
-                  Select category
-                </option>
-                {categories.map((category) => (
-                  <option key={category.value} value={category.value}>
-                    {category.label}
+      {isCreateModalOpen ? (
+        <form
+          onSubmit={saveData}
+          className={`p-4 items-center flex flex-col border-4 border-teal-800 m-4 `}
+        >
+          <div className="m-2">
+            <h2 className="text-white text-2xl mb-4 text-center">
+              Create New List For Your Menu
+            </h2>
+          </div>
+          <div className={styles.allInputWrapper}>
+            <div className={styles.selectCategoryInput}>
+              <h3>Category</h3>
+              <div className="selectWrapper">
+                <select
+                  value={menuCategoryValue}
+                  onChange={(e) => handleMenuCategoryChange(e.target.value)}
+                >
+                  <option className="flex justify-center" value="">
+                    Select category
                   </option>
-                ))}
-              </select>
-            </div>
-          </div>
-          <div className={styles.imgTextNameWrap}>
-            <Image
-              className="mr-2"
-              src={
-                !imageValue
-                  ? "https://github.com/levanydze/restaurant2024support/blob/main/icons/apple.png?raw=true"
-                  : imageValue
-              }
-              width={400}
-              height={300}
-              alt="any"
-            ></Image>
-            <div className="w-full flex flex-col justify-evenly">
-              <div className={` ${styles.textInputWrapper}`}>
-                <p>Image Link</p>
-                <input
-                  className="text-black m-1"
-                  type="text"
-                  value={imageValue}
-                  onChange={(e) => setImageValue(e.target.value)}
-                />
-              </div>
-              <div className={styles.textInputWrapper}>
-                <p>Name</p>
-                <input
-                  className="text-black m-1"
-                  type="text"
-                  value={nameValue}
-                  onChange={(e) => setNameValue(e.target.value)}
-                />
+                  {categories.map((category) => (
+                    <option key={category.value} value={category.value}>
+                      {category.label}
+                    </option>
+                  ))}
+                </select>
               </div>
             </div>
-          </div>
-          <div className={styles.textInputWrapper}>
-            <p>Description</p>
-            <input
-              className="text-black m-1"
-              type="text"
-              value={descriptionValue}
-              onChange={(e) => setDescriptionValue(e.target.value)}
-            />
-          </div>
-          <div className={styles.textInputWrapper}>
-            <p>Ingredients</p>
-            <input
-              className="text-black m-1"
-              type="text"
-              value={ingredientsValue}
-              onChange={(e) => setIngredientsValue(e.target.value)}
-            />
-          </div>
-          <div className={styles.textInputWrapper}>
-            <p>Portions</p>
-            <input
-              className="text-black m-1"
-              type="text"
-              value={portionsValue}
-              onChange={(e) => setPortionsValue(e.target.value)}
-            />
-          </div>
-          <div className={styles.textInputWrapper}>
-            <p>Price</p>
-            <input
-              className="text-black m-1"
-              type="number"
-              value={priceValue}
-              onChange={(e) => setPriceValue(parseFloat(e.target.value))}
-            />
-            <p>sek</p>
-          </div>
-          <div className={styles.checkBoxesWrapper}>
-            <div>
-              <p>Special</p>
+            <div className={styles.imgTextNameWrap}>
+              <Image
+                className="mr-2"
+                src={
+                  !imageValue
+                    ? "https://github.com/levanydze/restaurant2024support/blob/main/icons/apple.png?raw=true"
+                    : imageValue
+                }
+                width={400}
+                height={300}
+                alt="any"
+              ></Image>
+              <div className="w-full flex flex-col justify-evenly">
+                <div className={` ${styles.textInputWrapper}`}>
+                  <p>Image Link</p>
+                  <input
+                    className="text-black m-1"
+                    type="text"
+                    value={imageValue}
+                    onChange={(e) => setImageValue(e.target.value)}
+                  />
+                </div>
+                <div className={styles.textInputWrapper}>
+                  <p>Name</p>
+                  <input
+                    className="text-black m-1"
+                    type="text"
+                    value={nameValue}
+                    onChange={(e) => setNameValue(e.target.value)}
+                  />
+                </div>
+              </div>
+            </div>
+            <div className={styles.textInputWrapper}>
+              <p>Description</p>
               <input
-                type="checkbox"
-                checked={specialValue}
-                onChange={(e) => setSpecialValue(e.target.checked)}
+                className="text-black m-1"
+                type="text"
+                value={descriptionValue}
+                onChange={(e) => setDescriptionValue(e.target.value)}
               />
             </div>
-            <div>
-              <p>Season</p>
+            <div className={styles.textInputWrapper}>
+              <p>Ingredients</p>
               <input
-                type="checkbox"
-                checked={seasonValue}
-                onChange={(e) => setSeasonValue(e.target.checked)}
+                className="text-black m-1"
+                type="text"
+                value={ingredientsValue}
+                onChange={(e) => setIngredientsValue(e.target.value)}
               />
             </div>
-            <div>
-              <p>Vegan</p>
+            <div className={styles.textInputWrapper}>
+              <p>Portions</p>
               <input
-                type="checkbox"
-                checked={veganValue}
-                onChange={(e) => setVeganValue(e.target.checked)}
+                className="text-black m-1"
+                type="text"
+                value={portionsValue}
+                onChange={(e) => setPortionsValue(e.target.value)}
               />
             </div>
-            <div>
-              <p>Spicy</p>
+            <div className={styles.textInputWrapper}>
+              <p>Price</p>
               <input
-                type="checkbox"
-                checked={spicyValue}
-                onChange={(e) => setSpicyValue(e.target.checked)}
+                className="text-black m-1"
+                type="number"
+                value={priceValue}
+                onChange={(e) => setPriceValue(parseFloat(e.target.value))}
               />
+              <p>sek</p>
             </div>
-            <div>
-              <p>New</p>
-              <input
-                type="checkbox"
-                checked={newItemValue}
-                onChange={(e) => setNewItemValue(e.target.checked)}
-              />
-            </div>
-            {/* <div>
+            <div className={styles.checkBoxesWrapper}>
+              <div>
+                <p>Special</p>
+                <input
+                  type="checkbox"
+                  checked={specialValue}
+                  onChange={(e) => setSpecialValue(e.target.checked)}
+                />
+              </div>
+              <div>
+                <p>Season</p>
+                <input
+                  type="checkbox"
+                  checked={seasonValue}
+                  onChange={(e) => setSeasonValue(e.target.checked)}
+                />
+              </div>
+              <div>
+                <p>Vegan</p>
+                <input
+                  type="checkbox"
+                  checked={veganValue}
+                  onChange={(e) => setVeganValue(e.target.checked)}
+                />
+              </div>
+              <div>
+                <p>Spicy</p>
+                <input
+                  type="checkbox"
+                  checked={spicyValue}
+                  onChange={(e) => setSpicyValue(e.target.checked)}
+                />
+              </div>
+              <div>
+                <p>New</p>
+                <input
+                  type="checkbox"
+                  checked={newItemValue}
+                  onChange={(e) => setNewItemValue(e.target.checked)}
+                />
+              </div>
+              {/* <div>
               <p>Disabled?</p>
               <input
                 type="checkbox"
@@ -219,28 +221,48 @@ export default function CreateNewItem({
                 onChange={(e) => setDisableValue(e.target.checked)}
               />
             </div> */}
+            </div>
+            <div
+              className={`flex px-4 py-2 rounded-md justify-evenly w-72 m-auto mt-6 ${
+                disableValue ? " text-gray-400 bg-red-900" : " bg-teal-600 "
+              }`}
+            >
+              <p>
+                {" "}
+                Product Is {disableValue ? "Disabeled Now" : "Enabled Now"}
+              </p>
+              <input
+                className="h-6 w-6 my-auto"
+                type="checkbox"
+                checked={disableValue}
+                onChange={(e) => setDisableValue(e.target.checked)} // Update the updateNewItem state
+              />
+            </div>
           </div>
-          <div
-            className={`flex px-4 py-2 rounded-md justify-evenly w-72 m-auto mt-6 ${
-              disableValue ? " text-gray-400 bg-red-900" : " bg-teal-600 "
-            }`}
-          >
-            <p> Product Is {disableValue ? "Disabeled Now" : "Enabled Now"}</p>
-            <input
-              className="h-6 w-6 my-auto"
-              type="checkbox"
-              checked={disableValue}
-              onChange={(e) => setDisableValue(e.target.checked)} // Update the updateNewItem state
-            />
+          <div>
+            <button
+              className=" mx-3 bg-green-700 py-2 px-4 my-4 rounded-md"
+              type="submit"
+            >
+              SAVE NEW DATA
+            </button>
+            <button
+              className="mx-3 bg-red-700 py-2 px-4 my-4 rounded-md"
+              onClick={() => setIsCreateModalOpen(false)}
+            >
+              CANCEL
+            </button>
           </div>
-        </div>
-        <button
-          className="bg-green-700 py-2 px-4 my-4 rounded-md"
-          type="submit"
+        </form>
+      ) : (
+        <div
+          className="border-2 border-teal-700 w-[200px] h-[200px] m-auto my-[50px] flex  flex-col  cursor-pointer text-green-600"
+          onClick={() => setIsCreateModalOpen(true)}
         >
-          SAVE NEW DATA
-        </button>
-      </form>
+          <p className=" text-center text-[80px] ">+</p>
+          <p className=" text-center"> Add New Product</p>
+        </div>
+      )}
     </div>
   );
 }
